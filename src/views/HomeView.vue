37d0ef5d84a037d7abd409b46b983d5dfa15d448
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-01-30 01:04:31 +0300

    вопросы по конфигу

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,11 @@
 
 <template>
   <div>
+    <el-button>HELLO</el-button>
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <RouterLink :to="{ name: 'about' }">About</RouterLink>
   </div>
 </template>
      
