09006bcafc19f7a014a3493b6503c4b3381342de
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-01-30 00:10:23 +0300

    Добавил автоимпорт элементов

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    ElButton: typeof import('element-plus/es')['ElButton']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     }),
     Components({
+      dts: true,
       resolvers: [ElementPlusResolver()]
     })
   ],
