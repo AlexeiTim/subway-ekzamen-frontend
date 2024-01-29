b70f1411d20b87efcafe005fc9861d1902514991
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-01-29 23:39:15 +0300

    добавил vue-final-modal

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVfm } from 'vue-final-modal'
+
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import 'vue-final-modal/style.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const vfm = createVfm()
 
+app.use(vfm)
 app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
