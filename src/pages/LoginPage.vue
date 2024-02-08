9e3c2ea329aa7295d4216ef4ef81bd69b4004c3b
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-02-08 12:20:58 +0300

    работа с страницей авторизации

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex items-center justify-center h-full w-full">
+    <div class="rounded-xl bg-white xl:w-1/4 p-4 max-mobile:w-full">
+      <ElText tag="h1" class="text-center">Авторизация</ElText>
+      <ElForm :model="formData" label-position="top">
+        <ElFormItem label="Логин">
+          <ElInput v-model="formData.username" />
+        </ElFormItem>
+
+        <ElFormItem label="Пароль">
+          <ElInput v-model="formData.password" />
+        </ElFormItem>
+      </ElForm>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const formData = ref({
+  username: '',
+  password: ''
+})
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'mobile': '360px'
+    },
     extend: {},
   },
   plugins: [],
