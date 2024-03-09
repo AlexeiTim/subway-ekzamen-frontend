62d5aed8b442f9ecefe6f8363cced3acb80eb8b3
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-03-09 13:02:39 +0300

    изменил логику работы выхода из системы

--- src/components/Buttons/ButtonLogout.vue
+++ src/components/Buttons/ButtonLogout.vue
@@ -7,8 +7,7 @@
 <script lang="ts" setup>
 import { ROUTER_NAMES } from '@/constants/router';
 import { clearToken } from '@/services/api/config';
-import { AuthService } from '@/services/api/rest/auth';
-import { TokenStorage } from '@/services/storage/token';
+import { NotificationService } from '@/services/notify/notification';
 import { useUserStore } from '@/stores/user';
 import { useRouter } from 'vue-router';
 
@@ -17,26 +16,15 @@ const { logout } = useLogout()
 const handleLogout = async () => await logout()
 
 function useLogout() {
-  const authService = new AuthService()
-  const tokenStorage = new TokenStorage()
   const router = useRouter()
   const userStore = useUserStore()
+  const notificationService = new NotificationService()
 
   async function logout() {
-    console.log('init logout')
-    const token = tokenStorage.getToken()
-    console.log(token)
-
-    if (!token) return router.push({ name: ROUTER_NAMES.LOGIN })
-
-    try {
-      await authService.logout()
-      userStore.$reset()
-      clearToken()
-      router.push({ name: ROUTER_NAMES.LOGIN })
-    } catch (error) {
-      console.error(error)
-    }
+    userStore.$reset()
+    clearToken()
+    router.push({ name: ROUTER_NAMES.LOGIN })
+    notificationService.success('Ждём вашего возвращения')
   }
 
   return {
