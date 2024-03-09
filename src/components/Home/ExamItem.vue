557ea30c8358d46f70207decce80ad4f40bd6087
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-03-10 00:25:33 +0300

    Добавил компонент вывода блока экзамена

---
+++ src/components/Home/ExamItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-slate-400 border-2 p-2">
+    {{ props.exam.name }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { ExamModel } from '@/models/exam.model';
+
+interface Props {
+  exam: ExamModel
+}
+const props = defineProps<Props>()
+</script>
