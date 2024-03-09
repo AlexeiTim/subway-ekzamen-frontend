6ddc7134ced162a7589be5d3e1822623893ab08b
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-03-09 13:43:13 +0300

    Добавил валидацию, и оповещения для логинки

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -8,21 +8,29 @@
         Авторизация
       </ElText>
       <ElForm
+        ref="formRef"
         :model="loginData"
+        :rules="formRules"
         label-position="top"
       >
-        <ElFormItem label="Логин">
+        <ElFormItem
+          label="Логин"
+          prop="username"
+        >
           <ElInput v-model="loginData.username" />
         </ElFormItem>
 
-        <ElFormItem label="Пароль">
+        <ElFormItem
+          label="Пароль"
+          prop="password"
+        >
           <ElInput v-model="loginData.password" />
         </ElFormItem>
       </ElForm>
       <div class="flex items-center justify-between">
         <span>
           Ещё нет аккаунта?
-          <RouterLink :to="{ name: 'registration' }">
+          <RouterLink :to="{ name: ROUTER_NAMES.REGISTRATION }">
             <ElLink>Зарегистрироваться</ElLink>
           </RouterLink>
         </span>
@@ -39,22 +47,46 @@
 
 <script setup lang="ts">
 import { ERRORS } from '@/constants/errors';
-import { ROUTER_PATHES } from '@/constants/router';
-import UserModel from '@/models/user.model';
+import { ROUTER_NAMES } from '@/constants/router';
+import { generateWelcomeMessage } from '@/helpers/generateWelcomeMessage';
 import { setToken } from '@/services/api/config';
 import { AuthService } from '@/services/api/rest/auth';
 import { UserService } from '@/services/api/rest/user';
+import { NotificationService } from '@/services/notify/notification';
 import { useUserStore } from '@/stores/user';
+import validator from '@/utils/validator';
+import type { FormInstance, FormRules } from 'element-plus';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
+const formRef = ref<FormInstance>()
+
+const formRules = ref<FormRules>({
+  username: [{ validator: validator.notEmptyField, trigger: 'blur'}],
+  password: [{ validator: validator.notEmptyField, trigger: 'blur'}]
+})
+
 const { login, loginData } = useLogin()
 
-const handleLogin = async () => await login()
+async function checkValidForm() {
+  if (!formRef.value) return
+
+  const isValid = await formRef.value?.validate(isValid => isValid)
+  if (!isValid) return false
+  return true
+}
+
+const handleLogin = async () => {
+  const isValidForm = await checkValidForm()
+  if (!isValidForm) return
+
+  await login()
+}
 
 function useLogin() {
   const authService = new AuthService()
   const userService = new UserService()
+  const notificationService = new NotificationService()
   const userStore = useUserStore()
   const router = useRouter()
 
@@ -81,10 +113,13 @@ function useLogin() {
 
       if (!user) return alert('dont have user')
 
-      userStore.updateUser(new UserModel(user))
-      router.push(ROUTER_PATHES.HOME)
+      userStore.updateUser(user)
+      router.push({ name: ROUTER_NAMES.HOME })
+      
+      const welcomeMessage = generateWelcomeMessage(user.username)
+      notificationService.success(welcomeMessage)
     } catch (e) {
-      return console.log('проблемы с авторизацией')
+      return console.log(ERRORS.NOT_AUTHORIZATION)
     }
   }
 
@@ -93,4 +128,8 @@ function useLogin() {
     loginData
   }
 }
+
+defineExpose({
+  checkValidForm
+})
 </script>
