a30ebb5821a84ef7ff339a95562dfb9f4207efad
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-03-14 15:53:08 +0300

    Создал основной layout приложения

---
+++ src/layouts/LayoutApp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="flex flex-col h-full"
+    :style="{
+      backgroundImage: `url(${IMAGES.MAIN_BACKGROUND})`
+    }"
+  >
+    <div class="mb-2"> 
+      <slot name="header" />
+    </div>
+
+    <div class="max-w-[1240px] mx-auto flex-grow w-full px-1 py-1">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { IMAGES } from '@/constants/images';
+</script>
