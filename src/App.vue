ddf78e918bd885389ade7c54f8973027d7a69891
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-03-14 15:52:25 +0300

    Обернут в созданный основной layout приложения

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
-import AppHeader from '@/components/Common/AppHeader.vue'
-import { IMAGES } from '@/constants/images'
-import { ModalsContainer } from 'vue-final-modal'
+import AppHeader from '@/components/Common/AppHeader.vue';
+import LayoutApp from '@/layouts/LayoutApp.vue';
+import { ModalsContainer } from 'vue-final-modal';
 </script>
 
 <template>
-  <div
-    class="h-[100vh] flex flex-col"
-    :style="{
-      backgroundImage: `url(${IMAGES.MAIN_BACKGROUND})`
-    }"
-  >
-    <AppHeader class="mb-2" />
-    <RouterView class="flex-grow" />
-    <ModalsContainer />
-  </div>
+  <LayoutApp>
+    <template #header>
+      <AppHeader />
+    </template>
+    <template #content>
+      <RouterView />
+    </template>
+  </LayoutApp>
+  <ModalsContainer />
 </template>
