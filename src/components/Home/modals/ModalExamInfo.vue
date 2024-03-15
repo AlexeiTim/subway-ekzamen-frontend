dd1866647e7b4ad050c87a9a94eb24ff0fbaa6d1
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-03-15 04:10:34 +0300

    Добавил модальное окно отображения детальной инфы о экзамене

---
+++ src/components/Home/modals/ModalExamInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <VueFinalModalBase
+    :title="MODAL_TITLE"
+    @close="emits('close')"
+  >
+    <h1>{{ selectedExam?.name }}</h1>
+    <p>{{ examDescription }}</p>
+  </VueFinalModalBase>
+</template>
+
+<script lang="ts" setup>
+import VueFinalModalBase from '@/components/Modals/VueFinalModalBase.vue';
+import { useHomePageStore } from '@/stores/pages/home-page-store';
+import { computed } from 'vue';
+
+const MODAL_TITLE = 'Информация о экзамене'
+
+const { selectedExam } = useHomePageStore()
+
+const examDescription = computed(() => {
+  return selectedExam?.description || 'Информация отсутствует'
+})
+
+const emits = defineEmits<{
+  (e: 'close'): void
+}>()
+</script>
