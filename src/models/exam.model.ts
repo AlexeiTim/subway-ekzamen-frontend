4b5cbb94f62771d8e909f8a9256fd8029ba3eed0
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-03-15 04:11:35 +0300

    Обновил модельку экзамена

--- src/models/exam.model.ts
+++ src/models/exam.model.ts
@@ -3,9 +3,11 @@ import type { Exam } from "@/types/exam";
 export class ExamModel {
   readonly id: number;
   readonly name: string;
+  readonly description?: string;
 
-  constructor({ id, name }: Exam) {
+  constructor({ id, name, description = '' }: Exam) {
     this.id = id;
     this.name = name;
+    this.description = description
   }
 }
