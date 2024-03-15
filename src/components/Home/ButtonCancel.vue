4fe688e189bf28030f16473582540297ebfded95
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-03-15 04:09:39 +0300

    Добавил кнопку отмены выбранного экзамена

---
+++ src/components/Home/ButtonCancel.vue
@@ -0,0 +1,13 @@
+<template>
+  <ElButton @click="handleUnsetSelectedExam">
+    Отмена
+  </ElButton>
+</template>
+
+<script lang="ts" setup>
+import { useHomePageStore } from '@/stores/pages/home-page-store';
+
+const homePageStore = useHomePageStore()
+
+const handleUnsetSelectedExam = () => homePageStore.unsetSelectedExam()
+</script>
