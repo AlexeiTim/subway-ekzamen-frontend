80d152ce23219129690acfa8168386b219eaf691
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-03-18 01:44:31 +0300

    поднастроил конфиг, чтобы исключить лишние файлы для покрытия тестами

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath } from 'node:url'
-import { defineConfig, mergeConfig } from 'vitest/config'
+import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
@@ -12,10 +12,11 @@ export default mergeConfig(
         }
       },
       environment: 'jsdom',
-      // exclude: [...configDefaults.exclude, 'e2e/*'],
+      exclude: [...configDefaults.exclude, 'e2e/*', '**.config**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
       globals: true,
       coverage: {
+        exclude: ['**.config**', '**.d.**', '**.cjs', '**/assets', '**/types', '**/vite-env.d.ts'],
         provider: 'v8',
         thresholds: {
           lines: 80,
