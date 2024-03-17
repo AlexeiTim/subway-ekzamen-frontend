425113a5668e9da5118bf63e4c758151a1796910
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-03-17 20:37:16 +0300

    покрыл тестами страницу логина

--- src/spec/pages/LoginPage.spec.ts
+++ src/spec/pages/LoginPage.spec.ts
@@ -1,46 +1,120 @@
 import { useLogin } from "@/composables/useLogin";
 import LoginPage from "@/pages/LoginPage.vue";
-import type { AuthData } from "@/types/auth";
-import { mount, type VueWrapper } from "@vue/test-utils";
-import ElementPlus, { type FormInstance, type FormRules } from 'element-plus';
+import { flushPromises, mount } from "@vue/test-utils";
+import ElementPlus, { ElButton, ElInput } from 'element-plus';
 import { describe, expect, it, vi, type Mock } from "vitest";
 
 vi.mock('vue-router')
 vi.mock('@/composables/useLogin')
+const mockedLogin = vi.fn() as () => void
 
-interface ComponentSetup {
-  formRef: FormInstance,
-  formRules: FormRules,
-  login: () => Promise<void>,
-  loginData: AuthData,
-  checkValidForm: () => boolean,
-  handleLogin: () => Promise<void>,
-  useLogin: () => {
-    login: () => Promise<void>
-  },
-}
-
-type Wrapper = VueWrapper<ComponentSetup>
+(useLogin as Mock).mockReturnValue({
+  login: mockedLogin
+})
 
 describe('LoginPage', () => {
-  let wrapper: Wrapper
+  let wrapper: ReturnType<typeof mount>
 
-  function createComponent(options = {}, stubs = []) {
+  function findElementByTestId(name: string) {
+    return wrapper.find(`[data-testid="${name}"]`)
+  }
+
+  function createComponent({options = {}, stubs = {}, mocks = {}} = {}) {
     wrapper = mount(LoginPage, {
       ...options,
       global: {
         plugins: [ElementPlus],
-        stubs: ['RouterLink', 'ElInput', ...stubs],
+        stubs: {
+          teleport: true,
+          'RouterLink': {
+            template: '<div></div>',
+          },
+          ...stubs
+        },
+        mocks: {
+          ...mocks
+        }
       },
-    }) as Wrapper
+    }) as ReturnType<typeof mount> & typeof LoginPage
   }
 
   it('rendered', () => {
-    (useLogin as Mock).mockReturnValue({
-      login: vi.fn()
+    createComponent({
+
     })
-    createComponent()
 
     expect(wrapper.exists()).toBe(true)
   })
+
+  it('return if formRef is not have', async () => {
+    createComponent()
+    const result = await wrapper.vm.checkValidForm(null)
+
+    expect(result).toEqual(undefined)
+  })
+
+  describe('validation form', () => {
+    let usernameContainer
+    let passwordContainer
+    let inputs
+    let submitButton
+    let usernameInput
+    let passwordInput
+
+    it('login with empty login data', async () => {
+      createComponent()
+      usernameContainer = findElementByTestId('username-form-item')
+      passwordContainer = findElementByTestId('password-form-item')
+      inputs = wrapper.findAllComponents(ElInput)
+      submitButton = wrapper.findComponent(ElButton)
+      usernameInput = inputs[0]
+      passwordInput = inputs[1]
+
+      await submitButton.trigger('click')
+      await flushPromises()
+  
+      expect(usernameContainer.classes()).contain('is-error')
+      expect(passwordContainer.classes()).contain('is-error')
+      expect(mockedLogin).not.toHaveBeenCalled()
+    })
+  
+    it('login with empty username field and empty password field', async () => {
+      createComponent()
+      usernameContainer = findElementByTestId('username-form-item')
+      passwordContainer = findElementByTestId('password-form-item')
+      inputs = wrapper.findAllComponents(ElInput)
+      submitButton = wrapper.findComponent(ElButton)
+      usernameInput = inputs[0]
+      passwordInput = inputs[1]
+      usernameInput.setValue('1')
+      await submitButton.trigger('click')
+      await flushPromises()
+  
+      expect(usernameContainer.classes()).not.contain('is-error')
+      expect(passwordContainer.classes()).contain('is-error')
+      expect(mockedLogin).not.toHaveBeenCalled()
+    })
+
+    it('login with  username field and  password field', async () => {
+      createComponent()
+      usernameContainer = findElementByTestId('username-form-item')
+      passwordContainer = findElementByTestId('password-form-item')
+      inputs = wrapper.findAllComponents(ElInput)
+      submitButton = wrapper.findComponent(ElButton)
+      usernameInput = inputs[0]
+      passwordInput = inputs[1]
+      
+      usernameInput.setValue('1')
+      passwordInput.setValue('1')
+      await submitButton.trigger('click')
+      await flushPromises()
+  
+      expect(usernameContainer.classes()).not.contain('is-error')
+      expect(passwordContainer.classes()).not.contain('is-error')
+      expect(mockedLogin).toHaveBeenCalled()
+    })
+  })
+
+
+  
 })
