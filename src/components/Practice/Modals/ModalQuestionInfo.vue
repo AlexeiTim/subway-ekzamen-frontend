6a31b87e57318cc4852e182b2b7e9cf3e33d18d7
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-03-26 22:12:19 +0300

    Добавил модалку для отображения детальной информации о вопросе

---
+++ src/components/Practice/Modals/ModalQuestionInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <VueFinalModalBase
+    :title="MODAL_TITLE"
+    @close="emits('close')"
+  >
+    <h1>Content</h1>
+  </VueFinalModalBase>
+</template>
+
+<script lang="ts" setup>
+import VueFinalModalBase from '@/components/Modals/VueFinalModalBase.vue';
+
+const MODAL_TITLE = 'Информация о вопросе'
+const emits = defineEmits<{
+  (e: 'close'): void
+}>()
+</script>
