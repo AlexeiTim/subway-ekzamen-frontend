bcafacdf4107dc6a7d88046a7d9fedd9005ba9fa
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-04-10 12:45:37 +0300

    Сверстал страницу перед экзаменом

---
+++ src/pages/PrePracticePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex items-center justify-center h-[100%]">
+    <div class="text-center flex items-center justify-center flex-col gap-[124px]">
+      <div class="h-[100%]">
+        <h2
+          class="text-[82px] text-[#DBFF00]"
+        >
+          Внимание!
+        </h2>
+        <h2 class="text-[82px] text-[#DBFF00]">
+          Начинается проверка знаний.
+        </h2>
+        <h2 class="text-[82px] text-[#DBFF00]">
+          Желаем успеха!
+        </h2>
+      </div>
+
+      <div>
+        <p
+          class="text-white text-[32px]"
+        >
+          Вам будет предложено N вопросов
+        </p>
+        <p class="text-white text-[32px]">
+          К каждому вопросу имеется несколько вариантов ответа
+        </p>
+        <p class="text-white text-[32px]">
+          Выберите из них самый правильный или наиболее правильный ответ
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
