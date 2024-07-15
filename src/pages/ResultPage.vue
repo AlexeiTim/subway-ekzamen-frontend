a41b2b8db3c6834b5c0bcd52aff9fe6c6e5f5e4c
timashkov060896@gmail.com <timashkov060896@gmail.com>
2024-07-15 21:21:37 +0300

    добавил страницу результатов

---
+++ src/pages/ResultPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="size-full flex items-center justify-center">
+    <ElCard class="!bg-red-600 p-5 text-center">
+      <div class=" flex flex-col gap-5">
+        <h1>Не сдал</h1>
+        <p>Ошибок: N</p>
+        <div>
+          <ElButton>Проверить</ElButton>
+          <ElButton>Повторить</ElButton>
+          <ElButton @click="$router.push({ name: ROUTER_NAMES.HOME})">
+            Выход
+          </ElButton>
+        </div>
+      </div>
+    </ElCard>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ROUTER_NAMES } from '@/constants/router';
+</script>
